f629f7112dc29096be96b7a9bc1938909403f407
Filip Tijan <ftijan@gmail.com>
2023-09-17 00:49:59 +0100

    add solution, tpl package

---
+++ src/Example.TplDataflow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace Example.TplDataflow
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
