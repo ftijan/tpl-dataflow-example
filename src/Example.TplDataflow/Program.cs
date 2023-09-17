5d5a519326b94eb592831b27f842e23bb20502cd
Filip Tijan <ftijan@gmail.com>
2023-09-18 00:19:59 +0100

    batch block example

--- src/Example.TplDataflow/Program.cs
+++ src/Example.TplDataflow/Program.cs
@@ -4,10 +4,16 @@ internal class Program
 	static async Task Main(string[] args)
 	{
 		Console.WriteLine("Starting dataflow examples");
-
+		// 01
 		//ActionBlockExamples.ActionBlockExample();		
 		//await ActionBlockExamples.ActionBlockAsyncExample();
-		TransformBlockExamples.TransformBlockExample();
-		await TransformBlockExamples.TransformBlockAsyncExample();
+		// 02
+		//TransformBlockExamples.TransformBlockExample();
+		//await TransformBlockExamples.TransformBlockAsyncExample();
+		// 03
+		// Will throw:
+		//BatchBlockExamples.BatchBlockBadExample();
+		//BatchBlockExamples.BatchBlockFixedExample();
+		await BatchBlockExamples.BatchBlockFixedAsyncExample();
 	}	
 }	
